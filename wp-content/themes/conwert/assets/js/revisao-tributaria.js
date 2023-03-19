4399637d3c5da889db2f7a1f99f5bc7aa9e71604
Flávio <flavioczuk@gmail.com>
2023-03-19 09:24:52 -0300

    adding form validations to the new page

--- wp-content/themes/conwert/assets/js/revisao-tributaria.js
+++ wp-content/themes/conwert/assets/js/revisao-tributaria.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    // marquee text
     $('.marquee').marquee({
         direction: 'left',
         duration: 20000,
@@ -8,5 +9,29 @@ $(document).ready(function() {
         duplicated: true,
         startVisible: true
     });
+
+    // make the input placeholders smaller on focus
+    $('.line .input').focus(function(){
+        $(this).parent('.line').addClass('active');
+    });
+
+    $('.line .input').blur(function(){
+        if($(this).val() == ''){
+            $(this).parent('.line').removeClass('active');
+        }
+    });
+
+    // validate forms
+    $("#contact-form").validate();
+    $("#newsletter-form").validate();
+
+    // display success popups
+    if (window.location.href.indexOf("contato-sucesso") > -1) {
+        $.fancybox.open($("#contato-sucesso"));
+    }
+
+    if (window.location.href.indexOf("newsletter-sucesso") > -1) {
+        $.fancybox.open($("#newsletter-sucesso"));
+    }
 
 });
